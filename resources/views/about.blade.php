a52afb01ea20cc54967278bde09384624978fe7b
DhruvSvt <dhruvsvt08@gmail.com>
2024-03-09 13:11:10 +0530

    About us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,67 +1,72 @@
 @extends('master')
 
 @section('content')
-    <!-- How It Work -->
-    <section id="brdcrmb-bg-section" class="brdcrmb-bg "
-        style="background-image: url({{ url('assets/images/Memorial-Hermann-Header-background.jpg') }});background-blend-mode: multiply;">
+<!-- How It Work -->
+<section id="brdcrmb-bg-section" class="brdcrmb-bg "
+    style="background-image: url({{ url('assets/images/Memorial-Hermann-Header-background.jpg') }});background-blend-mode: multiply;">
 
 
-        <div class="brdcmb-and-ttl">
-            <div class="container">
-                <div class="brdcmb-wpr d-flx itm-cntr"><!-- Breadcrumb NavXT 6.6.0 -->
-                    <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to Home."
-                            href="{{ url('/') }}" class="home"><span property="name">Home</span></a>
-                        <meta property="position" content="1">
-                    </span><span class="brdcmb-arrow"></span><span property="itemListElement" typeof="ListItem"><a
-                            property="item" typeof="WebPage" title="Go to Mission And Vision." href="{{ url('/about-us') }}"
-                            class="post post-page"><span property="name">About Us</span></a>
-                </div>
-                <h1 style="margin-top: 40px;color: #fff;" class="page-ttl d-flx itm-cntr jstfy-cntr">About Us
-                </h1>
+    <div class="brdcmb-and-ttl">
+        <div class="container">
+            <div class="brdcmb-wpr d-flx itm-cntr">
+                <!-- Breadcrumb NavXT 6.6.0 -->
+                <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage"
+                        title="Go to Home." href="{{ url('/') }}" class="home"><span property="name">Home</span></a>
+                    <meta property="position" content="1">
+                </span><span class="brdcmb-arrow"></span><span property="itemListElement" typeof="ListItem"><a
+                        property="item" typeof="WebPage" title="Go to Mission And Vision." href="{{ url('/about-us') }}"
+                        class="post post-page"><span property="name">About Us</span></a>
             </div>
+            <h1 style="margin-top: 40px;color: #fff;" class="page-ttl d-flx itm-cntr jstfy-cntr">About Us
+            </h1>
         </div>
-    </section>
+    </div>
+</section>
 
-    <main>
-        <div class="page-cms  py-5">
-            <div class="container d-flx">
+<main>
+    <div class="page-cms  py-5">
+        <div class="container d-flx">
 
-                <div class="page-cntn-area w-100">
-                    <div class="row g-lg-5 g-3">
-                          <div class="col-md-5">
-                            <div id="hospital-loc-holder" class="location-info ms-lg-auto">
-                                <div class="card border-0 rounded-4 ah_shadow">
-                                    <img src="{{ url('assets/images/Vision-Mission.png') }}"
-                                        class="card-img-top p-2 rounded-4 img-fluid" alt="GR Hospital">
-                                </div>
+            <div class="page-cntn-area w-100">
+                <div class="row g-lg-5 g-3">
+                    <div class="col-md-5">
+                        <div id="hospital-loc-holder" class="location-info ms-lg-auto">
+                            <div class="card border-0 rounded-4 ah_shadow">
+                                <img src="{{ url('assets/images/aboutus.jpg') }}"
+                                    class="card-img-top p-2 rounded-4 img-fluid" alt="GR Hospital">
                             </div>
                         </div>
-                        <div class="col-md-7">
-                            <h3 class="page-sub-ttl">GR Hospital - Mission And Vision</h3>
-                            <h3 class="sub-ttl">Mission</h3>
-                            <p>Our mission is to provide exceptional, compassionate, and comprehensive healthcare services
-                                to our community. We are dedicated to delivering high-quality, patient-centered care that
-                                promotes wellness, restores health, and enhances the quality of life for all those we serve.
-                                We strive to uphold the highest standards of medical excellence, professionalism, and
-                                ethical conduct in every aspect of our practice.
-                            </p>
-
-                            <h3 class="sub-ttl mt-5">Vision</h3>
-                            <p>Our vision is to be the premier healthcare destination in our region, recognized for our
-                                commitment to excellence, innovation, and superior patient outcomes. We aspire to be a
-                                center of excellence where medical expertise, advanced technology, and compassionate care
-                                converge to transform lives and inspire hope. Through collaboration, education, and
-                                innovation, we aim to set new standards of excellence in healthcare delivery, while
-                                continuously adapting to meet the evolving needs of our community. Together, we envision a
-                                healthier, happier future for all.
-                            </p>
-                        </div>
-
                     </div>
+                    <div class="col-md-7">
+                        <h3 class="page-sub-ttl">GR Hospital - About Us</h3>
+
+                        <p>At GR Hospital, we are dedicated to providing compassionate and high-quality healthcare
+                            services to our community. With
+                            a commitment to excellence in patient care, our team of experienced medical professionals
+                            strives to meet the diverse
+                            needs of every individual who walks through our doors. From routine check-ups to complex
+                            surgeries, we prioritize
+                            patient well-being and comfort above all else.
+                        </p>
+
+
+                        <p>At the heart of GR Hospital lies a relentless pursuit of innovation and advancement in
+                            medical science. We continuously
+                            invest in state-of-the-art technology and employ evidence-based practices to deliver the
+                            best possible outcomes for our
+                            patients. Beyond medical expertise, we foster a supportive and caring environment where
+                            patients feel valued and
+                            respected throughout their healthcare journey. At GR Hospital, we stand as a beacon of hope
+                            and healing, dedicated to
+                            serving our community with integrity and compassion.
+                        </p>
+                    </div>
+
                 </div>
-
             </div>
-        </div>
 
-    </main>
+        </div>
+    </div>
+
+</main>
 @endsection
